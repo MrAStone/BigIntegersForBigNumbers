8b0e2f53d8ac3e9864423a84012762454137baaa
Al Stone <astone@heckgrammar.co.uk>
2024-10-10 15:57:31 +0100

    Add project files.

---
+++ BigIntegersForBigNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Numerics;
+namespace BigIntegersForBigNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long largerInteger;
+            
+            BigInteger reallyBigInteger = 1;
+            int n = 1000;
+
+            for(int i =1; i <= n; i++)
+            {
+                reallyBigInteger = reallyBigInteger * i;
+            }
+            Console.WriteLine(reallyBigInteger.ToString());
+
+        }
+    }
+}
